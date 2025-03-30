2b25337636ac670e084af9258b32cd7fcb8c0ac0
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2025-03-30 11:47:29 +0000

    Lancement Projet Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>First Page</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body>
+    <div class="h-screen flex justify-center items-center">
+        <p class="text-3xl font-bold">Hello World</p>
+    </div>
+</body>
+</html>

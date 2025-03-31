1c5fdb60942a7bc86f27c710e5e8224b5517d9b2
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2025-03-31 14:44:33 +0000

    fix error

--- routes/api.php
+++ routes/api.php
@@ -2,24 +2,21 @@
 
 use App\Http\Controllers\api\TaskController;
 use App\Http\Controllers\api\TasklistController;
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\api\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-// Route::resource('tasklists', \App\Http\Controllers\Api\TaskController::class);
 
 Route::resource('tasklists', TasklistController::class);
 Route::resource('tasks', TaskController::class);
 
 // Routes d'authentification
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 // Routes protégées par Sanctum
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', function (Request $request) {

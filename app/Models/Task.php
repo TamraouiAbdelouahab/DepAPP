d3405111c5f967375141ee3efaaddba1297877f5
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2025-04-17 10:31:48 +0100

    add priorite to table task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['title', 'description', 'status', 'important', 'creationDate', 'dueDate', 'tasklist_id'];
+    protected $fillable = ['title', 'description', 'status', 'important', 'creationDate', 'dueDate', 'tasklist_id','priorite'];
 
     public function taskList()
     {
